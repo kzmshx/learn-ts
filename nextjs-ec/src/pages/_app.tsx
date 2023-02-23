6ced2406a9981febc55b53ab1ca080786693372e
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-02-23 17:22:36 +0900

    nextjs-ec Setup eslint

--- nextjs-ec/src/pages/_app.tsx
+++ nextjs-ec/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
-import type {AppProps} from 'next/app'
-import Head from "next/head";
-import {createGlobalStyle} from "styled-components";
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   html, body, textarea {
@@ -26,20 +26,23 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-const MyApp = ({Component, pageProps}: AppProps) => {
-    return (
-        <>
-            <Head>
-                <meta key="charset" name="charset" content="utf-8"/>
-                <meta key="viewport" name="viewport"
-                      content="width=divice-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"/>
-                <meta property="og:locale" content="ja_JP"/>
-                <meta property="og:type" content="website"/>
-            </Head>
-            <GlobalStyle/>
-            <Component {...pageProps} />
-        </>
-    )
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta key="charset" name="charset" content="utf-8" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=divice-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+        />
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
